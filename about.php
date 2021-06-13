67bed979fd77cae794abb5ee187893bad5ba3481
monicaputriutami <81574673+monicaputriutami@users.noreply.github.com>
2021-06-14 02:07:05 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?= 'halaman about' ?></title>
+</head>
+<body>
+	<h1><?= 'halaman about' ?></h1>
+	<hr>
+	<p><?= 'ini adalah halaman about yang menjelaskan tentang isi halaman ini.' ?></p>
+</body>
+</html>
